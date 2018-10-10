70e3c16c580e58750fbea51b01f3b7ee7d228104
Ionică Bizău <bizauionica@gmail.com>
2018-10-10 12:10:52 -0500

    :arrow_up: 1.0.0 :tada:

---
+++ DOCUMENTATION.md
@@ -0,0 +1,14 @@
+## Documentation
+
+You can see below the API reference of this module.
+
+### `slugIt(input)`
+Accepts a string as input, and returns its slug.
+
+#### Params
+
+- **String** `input`: The input string to slugify.
+
+#### Return
+- **String** The slug.
+
--- example/index.js
+++ example/index.js
@@ -2,4 +2,18 @@
 
 const slugIt = require("../lib");
 
-console.log(slugIt());
+// Simple usage
+console.log(slugIt("Hello World"));
+
+// Using Romanian diacritics
+console.log(slugIt("Salutări din București!"));
+
+// Using special characters
+console.log(slugIt("I ♥ You!"));
+
+// Long dashes
+console.log(slugIt("2016 — What a year!"));
+
+// Add a new character to the charmap
+slugIt.charmap["😂"] = "joy"
+console.log(slugIt("I am full of 😂"));
