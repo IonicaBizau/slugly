932ec77e16dbf50fc76b8ca2c3b1dddc04a0a219
Ionică Bizău <bizauionica@gmail.com>
2018-10-10 19:41:48 -0500

    Use the blah config to show the description with url.

--- example/index.js
+++ example/index.js
@@ -4,16 +4,21 @@ const slugly = require("../lib");
 
 // Simple usage
 console.log(slugly("Hello World"));
+// => hello-world
 
 // Using Romanian diacritics
 console.log(slugly("Salutări din București!"));
+// => salutari-din-bucuresti
 
 // Using special characters
 console.log(slugly("I ♥ You!"));
+// => i-love-you
 
 // Long dashes
 console.log(slugly("2016 — What a year!"));
+// => 2016-what-a-year
 
 // Add a new character to the charmap
 slugly.charmap["😂"] = "joy"
 console.log(slugly("I am full of 😂"));
+// => i-am-full-of-joy
